b0b60f84106b22f5c94ba9b3d078f9afeb5d1258
vikas <vikaskathiat4508@gmail.com>
2026-04-10 18:18:09 +0530

    project structura ready env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vikas Singh",
+    author_email="Vikaskathiat4508@gmail.com",
+    package=find_packages()
+)
